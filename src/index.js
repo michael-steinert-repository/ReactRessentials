45502591621062b085e49fb3ca9ddf652a1e21ec
Michael Steinert <steinert-michael@gmx.de>
2020-04-06 22:19:51 +0200

    digital clock implemented

--- src/index.js
+++ src/index.js
@@ -4,10 +4,37 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+class Clock extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidUpdate() {
+    this.ticker = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.ticker);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Aktuelle Uhrzeit: {this.state.date.toLocaleTimeString()}</h1>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Clock/>,
   document.getElementById('root')
 );
 
