575bff917292df8235dc221d66e77b2e588f7c20
Michael Steinert <steinert-michael@gmx.de>
2020-04-07 08:31:52 +0200

    Multiple elements and events

--- src/index.js
+++ src/index.js
@@ -33,8 +33,52 @@ class Clock extends React.Component{
   }
 }
 
+class MagicTable extends React.Component {
+  crateTable() {
+    let table = [];
+    
+    for(let i = 0; i < 10; i++) {
+      let children = [];
+      table.push(<tr><td><MagicButton/></td></tr>);
+    }
+    return table;
+  }
+  render() {
+    return (
+      <table>
+        {this.crateTable()}
+      </table>
+    );
+  }
+}
+
+class MagicButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isRender:true};
+  }
+
+  deleteMe() {
+    this.setState({
+      isRender: false
+    });
+  }
+
+  render() {
+    if(this.state.isRender){
+      return (
+        <button onClick={(e) => this.deleteMe(e)}>
+          Delete me
+        </button>
+      );
+    } else {
+      return null;
+    }
+  }
+}
+
 ReactDOM.render(
-  <Clock/>,
+  <MagicTable/>,
   document.getElementById('root')
 );
 
