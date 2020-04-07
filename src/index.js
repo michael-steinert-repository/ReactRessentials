4216ec5e5048accf7cadb090e8347a2e18cbf606
Michael Steinert <steinert-michael@gmx.de>
2020-04-07 22:49:01 +0200

    Inputfields and live changes

--- src/index.js
+++ src/index.js
@@ -1,84 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-class Clock extends React.Component{
+
+class CurrencyInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {date: new Date()};
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  componentDidUpdate() {
-    this.ticker = setInterval(() => this.tick(), 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.ticker);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
+  handleChange(e) {
+    this.props.onCurrencyChange(e.target.value);
   }
 
   render() {
-    return (
+    const currency = this.props.curreny;
+    const value = this.props.value;
+    return(
+      <fieldset>
+        <legend>Gib die Menge in {currency} an:</legend>
+        <input value={value} onChange={this.handleChange}/>
+      </fieldset>
+    );
+  }
+}
+
+class CurrencyCalculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {currency: 'Euro', value: ''};
+    this.handleEuroChange = this.handleEuroChange.bind(this);
+    this.handleDollarChange = this.handleDollarChange.bind(this);
+  }
+
+  handleDollarChange(value) {
+    this.setState({currency: 'Dollar', value});
+  }
+
+  handleEuroChange(value) {
+    this.setState({currency: 'Euro', value});
+  }
+
+  render() {
+    const value = this.state.value;
+    const currency = this.state.currency;
+    let euro = 0;
+    let dollar = 0;
+
+    if(currency === "Euro") {
+      euro = value;
+      dollar = 1.14 * value;
+    } else {
+      dollar = value;
+      euro = 0.86 * value;
+    }
+
+    return(
       <div>
-        <h1>Aktuelle Uhrzeit: {this.state.date.toLocaleTimeString()}</h1>
+        <CurrencyInput currency="Euro" value={euro} onCurrencyChange={this.handleEuroChange}/>
+        <CurrencyInput currency="Dollar" value={dollar} onCurrencyChange={this.handleEuroChange}/>
       </div>
     );
   }
 }
 
-class MagicTable extends React.Component {
-  crateTable() {
-    let table = [];
-    
-    for(let i = 0; i < 10; i++) {
-      let children = [];
-      table.push(<tr><td><MagicButton/></td></tr>);
-    }
-    return table;
-  }
-  render() {
-    return (
-      <table>
-        {this.crateTable()}
-      </table>
-    );
-  }
-}
-
-class MagicButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {isRender:true};
-  }
-
-  deleteMe() {
-    this.setState({
-      isRender: false
-    });
-  }
-
-  render() {
-    if(this.state.isRender){
-      return (
-        <button onClick={(e) => this.deleteMe(e)}>
-          Delete me
-        </button>
-      );
-    } else {
-      return null;
-    }
-  }
-}
-
 ReactDOM.render(
-  <MagicTable/>,
+  <CurrencyCalculator/>,
   document.getElementById('root')
 );
 
