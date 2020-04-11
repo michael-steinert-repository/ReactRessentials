f17d89306442c010865f625d6937d35fe39eb7fe
Michael Steinert <steinert-michael@gmx.de>
2020-04-11 09:15:27 +0200

    redirect

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ function App() {
       <Route path='/' component={Clock}/>
       <Route path={['/CurrencyCalculator/', '/CurrencyCalculator/:currencyParam']} excat component={CurrencyCalculator}/>
       <Route path={['/MagicTable', '/Table']} excat component={MagicTable}/>
+      <Route path='/Table1'>
+        {42 == 42 ? <Redirect push to="/MagicTable"/> : <Clock/>} 
+      </Route>
     </Router>
   );
 }
